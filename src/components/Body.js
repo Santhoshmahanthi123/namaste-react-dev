e5ed90154aae06afbbdfdf8f02ddfc297071c042
Santhoshmahanthi123 <singumahanthisanthosh@gmail.com>
2024-07-17 20:40:14 +0530

    added useEffect and useState with swiggy API

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,10 +2,25 @@ import resList from "../utils/mockdata";
 import Restaurantcard from "./RestaurantCard";
 import resList from "../utils/mockdata";
 import { Button } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { resList } from "../utils/mockdata";
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.37240&lng=78.43780&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    let res =
+      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants;
+    res = res.map((restaurant) => restaurant.info);
+    setListOfRestaurants(res);
+    console.log(res);
+  };
+
   return (
     <div className="body">
       <div className="filter">
