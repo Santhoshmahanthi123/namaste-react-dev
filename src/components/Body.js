c288da7eb01147b294d2b1d0740d18efd4af84f3
Santhoshmahanthi123 <singumahanthisanthosh@gmail.com>
2024-07-27 19:04:47 +0530

    added search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,59 @@
 import Restaurantcard from "./RestaurantCard";
 import { Button } from "@mui/material";
+import { styled, alpha } from "@mui/material/styles";
 import { useEffect, useState } from "react";
 import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
 import Shimmer from "./Shimmer";
+import SearchIcon from "@mui/icons-material/Search";
+import InputBase from "@mui/material/InputBase";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  width: "100%",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
+
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [filteredRestaurant, setFilteredRestairant] = useState([]);
   useEffect(() => {
     fetchData();
   }, []);
@@ -20,7 +68,7 @@ const Body = () => {
         ?.restaurants;
     res = res.map((restaurant) => restaurant.info);
     setListOfRestaurants(res);
-    console.log(res);
+    setFilteredRestairant(res);
   };
 
   // conditional rendering using ternery operator
@@ -29,21 +77,54 @@ const Body = () => {
   ) : (
     <div className="body">
       <div className="filter">
-        <Button
-          variant="contained"
-          onClick={() => {
-            const filteredList = listOfRestaurants.filter(
-              (res) => res.avgRating > 4
-            );
-            setListOfRestaurants(filteredList);
-          }}
-          className="filter-btn"
-        >
-          Top Rated Restaurant
-        </Button>
+        <div className="search">
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+              className="search-box"
+              value={searchText}
+              onChange={(e) => {
+                setSearchText(e.target.value);
+              }}
+            />
+          </Search>
+          <Button
+            variant="contained"
+            onClick={() => {
+              console.log(searchText);
+
+              let filteredRestaurant = listOfRestaurants.filter((restaurant) =>
+                restaurant.name
+                  .toLowerCase()
+                  .includes(searchText.toLocaleLowerCase())
+              );
+              setFilteredRestairant(filteredRestaurant);
+              // filteredRestaurant = setListOfRestaurants(listOfRestaurants);
+            }}
+            className="filter-btn"
+          >
+            Search
+          </Button>
+          <Button
+            variant="contained"
+            onClick={() => {
+              const filteredList = listOfRestaurants.filter(
+                (res) => res.avgRating > 4
+              );
+              setFilteredRestairant(filteredList);
+            }}
+            className="filter-btn"
+          >
+            Top Rated Restaurant
+          </Button>
+        </div>
       </div>
       <div className="res-container">
-        {listOfRestaurants.map((restaurant) => (
+        {filteredRestaurant.map((restaurant) => (
           <Restaurantcard key={restaurant.id} resData={restaurant} />
         ))}
       </div>
