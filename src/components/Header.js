df021aa267d6405c19bcbf42a9b1ce31f2c6cc94
Santhoshmahanthi123 <singumahanthisanthosh@gmail.com>
2024-08-07 15:48:05 +0530

    added link router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
 export const Header = () => {
   const [btnName, setBtnName] = useState("Login");
   return (
@@ -10,9 +11,15 @@ export const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
           <li>Cart</li>
           <Button
             variant="contained"
