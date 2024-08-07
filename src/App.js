a8fc80b99b93cbc659269a480c4cd6edf5126720
Santhoshmahanthi123 <singumahanthisanthosh@gmail.com>
2024-08-07 15:28:33 +0530

    added error pages

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Header } from "./components/Header";
 import Body from "./components/Body";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useRouteError,
+} from "react-router-dom";
 import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 const AppLayout = () => {
   return (
     <div className="app">
@@ -17,10 +23,15 @@ const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    errorElement: <Error />,
   },
   {
     path: "/about",
     element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
   },
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
